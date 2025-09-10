6d9c3c4ed59e5ec4bbcd174211fac131feab41d0
MKato2361 <kojirou318jes@gmail.com>
2025-09-11 02:46:19 +0900

    service-worker.js を更新

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,36 @@
-// service-worker.js
+const CACHE_NAME = 'qr-app-v3'; // キャッシュバージョンを更新
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('qr-app-v1').then((cache) => {
+        caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll([
-                '.',
-                'style.css',
-                'script.js',
+                '/',
+                '/index.html',
+                '/style.css',
+                '/script.js',
                 'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
-                'manifest.json',
-                'icon-192x192.png',
-                'icon-512x512.png'
+                '/manifest.json',
+                '/icon-180x180.png',
+                '/icon-192x192.png',
+                '/icon-512x512.png',
+                '/splash-640x1136.png'
             ]);
         })
     );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
